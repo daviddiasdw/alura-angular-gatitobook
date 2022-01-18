7944c1bb1a10ff7de31329b3bd3f684924163c4a
David William <daviddiasdw@gmail.com>
2022-01-18 12:59:34 -0300

    Importando novos modulos e declaração de novos components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,27 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { MessageModule } from '../components/message/message.module';
+import { MessageComponent } from '../components/message/message.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NewUserComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule,
+    MessageModule,
+    ReactiveFormsModule
   ],
   exports: [HomeComponent]
 })
