694571c38b9f2fa64c21cb1bcbe5bb8aa0a0b138
David William <daviddiasdw@gmail.com>
2022-01-18 12:51:17 -0300

    Corrigindo URL

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  constructor(private httpCliente:HttpClient) { }
 
-  constructor() { }
+  authenticate(user: string, password: string): Observable<any> {
+    return this.httpCliente.post('http://localhost:3000/user/login', {
+      userName: user,
+      password: password
+    })
+  }
 }
